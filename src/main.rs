105b2d6c5c3847fd0d35855e8baeb22e2a2d625c
Julien Castelain <julien.castelain@liferay.com>
2021-10-19 15:08:26 +0200

    Draw a triangle
    Taken from
    https://github.com/brendanzab/gl-rs/blob/master/gl/examples/triangle.rs
    but at least shows how to get started with OpenGL
    in Rust ... What used to be very verbose in C is even
    more verbose, but that's about it

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,97 @@
 extern crate gl;
 
+use gl::types::*;
+
 use glutin::event::{Event, WindowEvent};
 use glutin::event_loop::{ControlFlow, EventLoop};
 use glutin::window::WindowBuilder;
 use glutin::ContextBuilder;
+
+use std::ffi::CString;
+use std::mem;
+use std::ptr;
+use std::str;
+
+static VERTEX_DATA: [GLfloat; 6] = [0.0, 0.5, 0.5, -0.5, -0.5, -0.5];
+
+static VS_SRC: &'static str = "
+#version 150
+in vec2 position;
+void main() {
+    gl_Position = vec4(position, 0.0,  1.0);
+}";
+
+static FS_SRC: &'static str = "
+#version 150
+out vec4 out_color;
+
+void main() {
+    out_color = vec4(1.0, 1.0, 1.0, 1.0);
+}";
+
+fn compile_shader(src: &str, ty: GLenum) -> GLuint {
+    unsafe {
+        let shader = gl::CreateShader(ty);
+        let c_str = CString::new(src.as_bytes()).unwrap();
+        gl::ShaderSource(shader, 1, &c_str.as_ptr(), ptr::null());
+        gl::CompileShader(shader);
+
+        let mut status = gl::FALSE as GLint;
+        gl::GetShaderiv(shader, gl::COMPILE_STATUS, &mut status);
+
+        if status != (gl::TRUE as GLint) {
+            let mut len = 0;
+            gl::GetShaderiv(shader, gl::INFO_LOG_LENGTH, &mut len);
+            let mut buf = Vec::with_capacity(len as usize);
+            buf.set_len((len as usize) - 1);
+            gl::GetShaderInfoLog(
+                shader,
+                len,
+                ptr::null_mut(),
+                buf.as_mut_ptr() as *mut GLchar,
+            );
+            panic!(
+                "{}",
+                str::from_utf8(&buf)
+                    .ok()
+                    .expect("shader info log not valid utf8")
+            );
+        }
+        shader
+    }
+}
+
+fn link_program(vs: GLuint, fs: GLuint) -> GLuint {
+    unsafe {
+        let program = gl::CreateProgram();
+        gl::AttachShader(program, vs);
+        gl::AttachShader(program, fs);
+        gl::LinkProgram(program);
+
+        let mut status = gl::FALSE as GLint;
+        gl::GetProgramiv(program, gl::LINK_STATUS, &mut status);
+
+        if status != (gl::TRUE as GLint) {
+            let mut len: GLint = 0;
+            gl::GetProgramiv(program, gl::INFO_LOG_LENGTH, &mut len);
+            let mut buf = Vec::with_capacity(len as usize);
+            buf.set_len((len as usize) - 1);
+            gl::GetProgramInfoLog(
+                program,
+                len,
+                ptr::null_mut(),
+                buf.as_mut_ptr() as *mut GLchar,
+            );
+            panic!(
+                "{}",
+                str::from_utf8(&buf)
+                    .ok()
+                    .expect("program info log not valid utf8")
+            );
+        }
+        program
+    }
+}
 
 fn main() {
     let el = EventLoop::new();
@@ -14,18 +102,64 @@ fn main() {
 
     gl::load_with(|symbol| context.get_proc_address(symbol));
 
+    let vs = compile_shader(VS_SRC, gl::VERTEX_SHADER);
+    let fs = compile_shader(FS_SRC, gl::FRAGMENT_SHADER);
+    let program = link_program(vs, fs);
+
+    let mut vao = 0;
+    let mut vbo = 0;
+
+    #[allow(temporary_cstring_as_ptr)]
+    unsafe {
+        gl::GenVertexArrays(1, &mut vao);
+        gl::BindVertexArray(vao);
+
+        gl::GenBuffers(1, &mut vbo);
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            (VERTEX_DATA.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+            mem::transmute(&VERTEX_DATA[0]),
+            gl::STATIC_DRAW,
+        );
+
+        gl::UseProgram(program);
+        gl::BindFragDataLocation(program, 0, CString::new("out_color").unwrap().as_ptr());
+
+        let pos_attr = gl::GetAttribLocation(program, CString::new("position").unwrap().as_ptr());
+        gl::EnableVertexAttribArray(pos_attr as GLuint);
+        gl::VertexAttribPointer(
+            pos_attr as GLuint,
+            2,
+            gl::FLOAT,
+            gl::FALSE as GLboolean,
+            0,
+            ptr::null(),
+        );
+    }
+
     el.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
         match event {
             Event::LoopDestroyed => {}
             Event::WindowEvent { event, .. } => match event {
-                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                WindowEvent::CloseRequested => {
+                    unsafe {
+                        gl::DeleteProgram(program);
+                        gl::DeleteShader(vs);
+                        gl::DeleteShader(fs);
+                        gl::DeleteBuffers(1, &vbo);
+                        gl::DeleteVertexArrays(1, &vao);
+                    }
+                    *control_flow = ControlFlow::Exit
+                }
                 _ => (),
             },
             Event::RedrawRequested(_) => {
                 unsafe {
                     gl::ClearColor(0.3, 0.3, 0.3, 1.0);
                     gl::Clear(gl::COLOR_BUFFER_BIT);
+                    gl::DrawArrays(gl::TRIANGLES, 0, 3);
                 }
                 context.swap_buffers().unwrap();
             }
