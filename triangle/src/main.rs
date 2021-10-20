a2d6b7842f73f3b03dac5481379ceed91b329b9b
Julien Castelain <julien.castelain@liferay.com>
2021-10-20 20:53:07 +0200

    Use cargo workspaces
    Not sure if this is the way to go with
    multiple binaries, but I'll try and see

--- triangle/src/main.rs
+++ triangle/src/main.rs
@@ -12,6 +12,8 @@ use std::mem;
 use std::ptr;
 use std::str;
 
+use utils;
+
 static VERTEX_DATA: [GLfloat; 6] = [0.0, 0.5, 0.5, -0.5, -0.5, -0.5];
 
 static VS_SRC: &'static str = "
@@ -29,70 +31,6 @@ void main() {
     out_color = vec4(1.0, 1.0, 1.0, 1.0);
 }";
 
-fn compile_shader(src: &str, ty: GLenum) -> GLuint {
-    unsafe {
-        let shader = gl::CreateShader(ty);
-        let c_str = CString::new(src.as_bytes()).unwrap();
-        gl::ShaderSource(shader, 1, &c_str.as_ptr(), ptr::null());
-        gl::CompileShader(shader);
-
-        let mut status = gl::FALSE as GLint;
-        gl::GetShaderiv(shader, gl::COMPILE_STATUS, &mut status);
-
-        if status != (gl::TRUE as GLint) {
-            let mut len = 0;
-            gl::GetShaderiv(shader, gl::INFO_LOG_LENGTH, &mut len);
-            let mut buf = Vec::with_capacity(len as usize);
-            buf.set_len((len as usize) - 1);
-            gl::GetShaderInfoLog(
-                shader,
-                len,
-                ptr::null_mut(),
-                buf.as_mut_ptr() as *mut GLchar,
-            );
-            panic!(
-                "{}",
-                str::from_utf8(&buf)
-                    .ok()
-                    .expect("shader info log not valid utf8")
-            );
-        }
-        shader
-    }
-}
-
-fn link_program(vs: GLuint, fs: GLuint) -> GLuint {
-    unsafe {
-        let program = gl::CreateProgram();
-        gl::AttachShader(program, vs);
-        gl::AttachShader(program, fs);
-        gl::LinkProgram(program);
-
-        let mut status = gl::FALSE as GLint;
-        gl::GetProgramiv(program, gl::LINK_STATUS, &mut status);
-
-        if status != (gl::TRUE as GLint) {
-            let mut len: GLint = 0;
-            gl::GetProgramiv(program, gl::INFO_LOG_LENGTH, &mut len);
-            let mut buf = Vec::with_capacity(len as usize);
-            buf.set_len((len as usize) - 1);
-            gl::GetProgramInfoLog(
-                program,
-                len,
-                ptr::null_mut(),
-                buf.as_mut_ptr() as *mut GLchar,
-            );
-            panic!(
-                "{}",
-                str::from_utf8(&buf)
-                    .ok()
-                    .expect("program info log not valid utf8")
-            );
-        }
-        program
-    }
-}
-
 fn main() {
     let el = EventLoop::new();
     let wb = WindowBuilder::new();
@@ -102,9 +40,9 @@ fn main() {
 
     gl::load_with(|symbol| context.get_proc_address(symbol));
 
-    let vs = compile_shader(VS_SRC, gl::VERTEX_SHADER);
-    let fs = compile_shader(FS_SRC, gl::FRAGMENT_SHADER);
-    let program = link_program(vs, fs);
+    let vs = utils::compile_shader(VS_SRC, gl::VERTEX_SHADER);
+    let fs = utils::compile_shader(FS_SRC, gl::FRAGMENT_SHADER);
+    let program = utils::link_program(vs, fs);
 
     let mut vao = 0;
     let mut vbo = 0;
